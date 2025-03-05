2569489755701f14923e687ed44bd4951c15d257
TheMakerMahes <umamaheswaran46@gmail.com>
2025-03-05 21:08:51 +0000

    FIX:Sum of The First and Last Digit Number

--- SumOfFirstAndLastDigit.c
+++ SumOfFirstAndLastDigit.c
@@ -1,7 +1,4 @@
 /*
-SumOfFirstAndLastDigit.c
-*/
-/*
 If a Four Digit Number is input through the Keyboard,Write a Program obtain the sum 
 of the first and last digit of this Number ?
 */
@@ -10,14 +7,14 @@ int main()
 {
     int a,b,c,d,e,f,g,sum=0;
     printf("Enter the Four Digit Number : ");
-    scanf("%d",&a);//1234
-    b=a/10;//123
-    c=a%10;//4
-    sum+=c;//4
-    d=b/10;//12
-    e=b%10;//3
-    f=d/10;//1
-    g=d%10;//2
+    scanf("%d",&a);
+    b=a/10;
+    c=a%10;
+    sum+=c;
+    d=b/10;
+    e=b%10;
+    f=d/10;
+    g=d%10;
     sum+=f;
     printf("Sum of the First and Last Digit Number is %d : %d",a,sum);
 
